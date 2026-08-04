0b72f84e95a94d06960d71f26f39fd1e09d6cfec
S Venkatesh <216847616+venkatesh10s@users.noreply.github.com>
2026-08-04 19:11:02 +0530

    day1changes

---
+++ practice/prime.py
@@ -0,0 +1,11 @@
+num = int(input("enter a number:"))
+count = 0
+
+for i in range(1, num + 1):
+    if num % i == 0:
+        count+=1
+
+if count == 2:
+    print("prime")
+else:
+    print("not prime")
